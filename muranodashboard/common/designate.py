af2270ba2d0e7439eb22c248dc2672f766246880
Andy Botting <andy@andybotting.com>
2019-01-18 09:30:20 +1100

    Add designate support for Zone choice
    This commit adds support for Designate zones. This can be used to
    provide the user a drop down list of zones they can choose from when
    launching apps.
    
    This should be coupled with a commit to murano to add a new
    io.murano.resources.RecordSet resource type, so that new a new DNS
    record can be added for new apps.
    
    Change-Id: I71deb3e264b2777ec4126d8e46c3ac45b9fc42c6

---
+++ muranodashboard/common/designate.py
@@ -0,0 +1,64 @@
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+from designateclient.v2 import client as designate_client
+from django.conf import settings
+
+from horizon.utils.memoized import memoized_with_request
+
+from keystoneauth1 import loading
+from keystoneauth1 import session
+
+from openstack_dashboard.api import base
+from oslo_log import log as logging
+
+LOG = logging.getLogger(__name__)
+
+
+def get_auth_params_from_request(request):
+    """Extracts properties needed by designateclient from the request object.
+
+    These will be used to memoize the calls to designateclient.
+    """
+    return (
+        request.user.token.id,
+        base.url_for(request, 'dns'),
+        base.url_for(request, 'identity')
+    )
+
+
+@memoized_with_request(get_auth_params_from_request)
+def designateclient(request_auth_params):
+    (
+        token_id,
+        designate_url,
+        auth_url
+    ) = request_auth_params
+
+    insecure = getattr(settings, 'OPENSTACK_SSL_NO_VERIFY', False)
+    cacert = getattr(settings, 'OPENSTACK_SSL_CACERT', None)
+
+    loader = loading.get_plugin_loader('token')
+    auth = loader.load_from_options(auth_url=auth_url, token=token_id,
+                                    insecure=insecure, cacert=cacert)
+    sess = session.Session(auth=auth)
+    d = designate_client.Client(session=sess)
+    return d
+
+
+def zone_list(request):
+    d_client = designateclient(request)
+    if d_client is None:
+        return []
+    return d_client.zones.list()
